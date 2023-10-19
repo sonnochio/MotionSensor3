d3042f094530826a76c96e5cf083bcb4d267e646
Sonny Kong <whysonnykong@gmail.com>
2023-10-19 14:33:03 +0100

    Initial Commit

---
+++ MotionSensor3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MotionSensor3
+//
+//  Created by Sonny Kong on 19/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
